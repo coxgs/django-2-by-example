9948f0aecea8546d06105ac923bcb1632c4f1787
Geoff Cox <slangingcode@gmail.com>
2019-04-24 12:58:43 -0400

    Password reset via email.

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -125,3 +125,7 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
+
+# SMTP configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
